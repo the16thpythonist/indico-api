5630819437467b0a0a18fe4a074250d1c6cc0be2
the16thpythonist <jonseb1998@gmail.com>
2018-07-16 14:19:01 +0200

    0.0.0.1

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 - Initial commit
 
+### 0.0.0.1 - 16.07.2018
+
+- Fixed the bug with the DateTime creation in the EventArgumentAdapter not working due to a false format string
--- src/the16thpythonist/Indico/EventArgumentAdapter.php
+++ src/the16thpythonist/Indico/EventArgumentAdapter.php
@@ -10,8 +10,19 @@ namespace the16thpythonist\Indico;
 
 use DateTime;
 
+/**
+ * Class EventArgumentAdapter
+ *
+ * Takes the array from the indico response and turns it into an array of arguments, that can be used directly to be
+ * passed to the constructor of an Event object.
+ *
+ * @package the16thpythonist\Indico
+ */
 class EventArgumentAdapter
 {
+    /**
+     * @var array the array structure, directly from the indico api response
+     */
     public $entry;
 
     public function __construct(array $response_entry)
@@ -19,6 +30,17 @@ class EventArgumentAdapter
         $this->entry = $response_entry;
     }
 
+    /**
+     * Adapts the arguments of the response array so that they can be used for a Event object creation directly
+     *
+     * CHANGELOG
+     *
+     * Added 10.07.2018
+     *
+     * @since 0.0.0.0
+     *
+     * @return array
+     */
     public function getArgs(): array {
         $args = $this->entry;
         $args['creator']['name'] = $args['creator']['fullName'];
@@ -28,9 +50,24 @@ class EventArgumentAdapter
         return $args;
     }
 
+    /**
+     * Creates a DateTime object from the array structure of Date entries in the indico response
+     *
+     * CHANGELOG
+     *
+     * Added 10.07.2018
+     *
+     * Changed 16.07.2018
+     *
+     * The format string, which was used for the creation of the DateTime object was wrong. The symbol 'i' stands for
+     * minutes and not 'H'. This would result in the method just returning False.
+     *
+     * @param array $args
+     * @return bool|DateTime
+     */
     private function createDateTime(array $args) {
         $string = $args['date'] . ' ' . $args['time'];
-        $datetime = DateTime::createFromFormat('Y-m-d H:M:s', $string);
+        $datetime = DateTime::createFromFormat('Y-m-d H:i:s', $string);
         return $datetime;
     }
 }
